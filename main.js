6f8ff94814325d26dad05ea8a59149d19efc850d
Shubhak5 <83910260+Shubhak5@users.noreply.github.com>
2021-05-26 17:33:26 +0530

    Final code for ADV C56 Project as on 26 May

---
+++ main.js
@@ -0,0 +1,86 @@
+function setDate() {
+    document.getElementById("headDate").innerHTML = new Date().toDateString();
+}
+
+function highlight(imgNum) {
+    if (imgNum == 1) {
+        document.getElementById("imgFood1").style.borderColor = "darkred";
+        document.getElementById("imgFood2").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood3").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood4").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood5").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood6").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood7").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood8").style.borderColor = "darkgoldenrod";
+    }
+    else if (imgNum == 2) {
+        document.getElementById("imgFood1").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood2").style.borderColor = "darkred";
+        document.getElementById("imgFood3").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood4").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood5").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood6").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood7").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood8").style.borderColor = "darkgoldenrod";
+    }
+    else if (imgNum == 3) {
+        document.getElementById("imgFood1").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood2").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood3").style.borderColor = "darkred";
+        document.getElementById("imgFood4").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood5").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood6").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood7").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood8").style.borderColor = "darkgoldenrod";
+    }
+    else if (imgNum == 4) {
+        document.getElementById("imgFood1").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood2").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood3").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood4").style.borderColor = "darkred";
+        document.getElementById("imgFood5").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood6").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood7").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood8").style.borderColor = "darkgoldenrod";
+    }
+    else if (imgNum == 5) {
+        document.getElementById("imgFood1").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood2").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood3").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood4").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood5").style.borderColor = "darkred";
+        document.getElementById("imgFood6").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood7").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood8").style.borderColor = "darkgoldenrod";
+    }
+    else if (imgNum == 6) {
+        document.getElementById("imgFood1").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood2").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood3").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood4").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood5").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood6").style.borderColor = "darkred";
+        document.getElementById("imgFood7").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood8").style.borderColor = "darkgoldenrod";
+    }
+    else if (imgNum == 7) {
+        document.getElementById("imgFood1").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood2").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood3").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood4").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood5").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood6").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood7").style.borderColor = "darkred";
+        document.getElementById("imgFood8").style.borderColor = "darkgoldenrod";
+    }
+    else if (imgNum == 8) {
+        document.getElementById("imgFood1").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood2").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood3").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood4").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood5").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood6").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood7").style.borderColor = "darkgoldenrod";
+        document.getElementById("imgFood8").style.borderColor = "darkred";
+    }
+}
